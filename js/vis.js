c3a271c03f8b962aec8bbe24df73557dcff58749
emilyfuhrman <emily.c.fuhrman@gmail.com>
2016-06-21 18:42:16 -0400

    starting on a flawed hexmesh

--- js/vis.js
+++ js/vis.js
@@ -6,6 +6,9 @@ var init = function(){
 
 		mode:'math',
 
+		w:window.innerWidth,
+		h:window.innerHeight,
+
 		hex_file_w:30,
 		hex_file_h:30,
 		hex_w:30,
@@ -37,7 +40,7 @@ var init = function(){
 			//store (basic) position of each data point
 			//doesn't take into account actual size; just distance from origin
 			//thank you, http://www.redblobgames.com/grids/hexagons/
-			var origin = {
+			/*var origin = {
 				'x':window.innerWidth/2,
 				'y':window.innerHeight/2
 			};
@@ -69,16 +72,19 @@ var init = function(){
 				d.pos.x -=self.hex_w/2;
 				d.pos.y -=self.hex_h/2;
 				//console.log(d.pos);
-			});
+			});*/
 
 			self.generate();
 		},
 		generate:function(){
+			self.w = window.innerWidth;
+			self.h = window.innerHeight;
+
 			self.svg = d3.select('#container').append('svg')
 				.style('background',self.colors[self.mode]);
 
 			//maximum radius of the hexagon group
-			var max_vis_r = window.innerHeight/3;
+			//var max_vis_r = window.innerHeight/3;
 
 			//calculate the size an individual hexagon should be
 			//based on the length of the full dataset
@@ -86,22 +92,39 @@ var init = function(){
 
 			}
 
+			var origin = {
+				'x':window.innerWidth/2,
+				'y':window.innerHeight/2
+			};
+			var hexbin = d3.hexbin()
+				.size([self.h,self.w])
+				.radius(20);
+
 			var hexG,
+				hexmesh,
 				hexes;
 			hexG = self.svg.selectAll('g.hexG')
 				.data([self.data]);
 			hexG.enter().append('g')
 				.classed('hexG',true);
+			hexG
+				.attr('transform','translate(' +self.w +',0)rotate(90)');
 			hexG.exit().remove();
+			hexmesh = hexG.selectAll('path.hexmesh')
+				.data([self.data]);
+			hexmesh.enter().append('path')
+				.classed('hexmesh',true);
+			hexmesh
+				.attr('d',hexbin.mesh);
+			hexmesh.exit().remove();
 			hexes = hexG.selectAll('path.hex')
 				.data(function(d){ return d; });
 			hexes.enter().append('path')
 				.classed('hex',true);
 			hexes
-				.attr('d',self.path_hex)
+				.attr('d',hexbin.hexagon())
 				.attr('transform',function(d,i){
-					//var pos = calc_hexpos(d,i);
-					return 'translate(' +d.pos.x +',' +d.pos.y +')';
+					return 'translate(' +d +')';
 				})
 				.style('stroke',self.colors[self.mode])
 				.style('fill-opacity',function(d){
