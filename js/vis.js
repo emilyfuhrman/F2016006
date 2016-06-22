aa657688d63745d5e95e455fc2172c23a46d8b1d
emilyfuhrman <emily.c.fuhrman@gmail.com>
2016-06-22 15:12:40 -0400

    hexagon magnifier

--- js/vis.js
+++ js/vis.js
@@ -4,7 +4,11 @@ var init = function(){
 
 		data:null,
 
+		//default mode
 		mode:'math',
+
+		//default view
+		view:0,
 
 		w:window.innerWidth,
 		h:window.innerHeight,
@@ -31,13 +35,13 @@ var init = function(){
 		processData:function(){
 
 			//make rating into number
-			//set placeholder positions
+			//create position placeholders
 			self.data.forEach(function(d){
 				d.rating = +d.rating;
 
 				d.pos = {};
-				d.pos.cube = {};
-				d.pos.pixel  = {};
+				d.pos.cube  = {};
+				d.pos.pixel = {};
 			});
 			self.data.sort(function(a,b){
 				return d3.descending(a.rating,b.rating);
@@ -54,25 +58,20 @@ var init = function(){
 			self.svg = d3.select('#container').append('svg')
 				.style('background',self.colors[self.mode]);
 
-			//**TODO
-			//maximum radius of the hexagon group
-			//var hex_rad = window.innerHeight/3;
-			//hardcode for now
-			var hex_rad = 9;
+			//**TODO -- determine number of rings to calculate positions for
 
-			//calculate the size an individual hexagon should be
-			//based on the length of the full dataset
-			function calc_hexSize(){
+			//**TODO -- determine how to size hexagons in order for the group to fit nicely on a screen
 
-			}
+			//**TODO -- calculate correct radius for hexagon group
+			var hex_rad = 8;
 
 			//convert cube coordinates to pixel coordinates
 			function coords_cube_to_pix(_cube){
 				var obj = {};
 				var s = hex_rad;
 
-				obj.x = _cube.x%1 === 1 ? Math.sqrt(3) * s * (_cube.z/2 +_cube.x) : -Math.sqrt(3) * s * (_cube.z/2 +_cube.y);
-				obj.y = 3/2 * s * _cube.z;
+				obj.x = s * Math.sqrt(3) * (_cube.x +_cube.z/2);
+				obj.y = s * 3/2 * _cube.z;
 
 				// thank you, http://stackoverflow.com/questions/2459402/hexagonal-grid-coordinates-to-pixel-coordinates?rq=1
 				// y = 3/2 * s * b
@@ -86,9 +85,6 @@ var init = function(){
 
 				return obj;
 			}
-
-			//this is really just for getting a hexagon path
-			var hexbin = d3.hexbin().radius(hex_rad);
 
 			//generate cube coordinates for data points
 			//thank you, http://stackoverflow.com/questions/2049196/generating-triangular-hexagonal-coordinates-xyz
@@ -110,38 +106,88 @@ var init = function(){
 				d.pos.pixel = coords_cube_to_pix(cube_coords[i]);
 			});
 
+			//hexagon logic
+			//this is just used to neatly generate a hexagon path
+			var hexbin = d3.hexbin().radius(hex_rad),
+				hexbinLG = d3.hexbin().radius(hex_rad*2);
+			var hexTTG,
+				hexTTback,
+				hexTT;
 			var hexG,
-				hexmesh,
+				hexesG,
 				hexes;
+
 			hexG = self.svg.selectAll('g.hexG')
 				.data([self.data]);
 			hexG.enter().append('g')
 				.classed('hexG',true);
 			hexG
 				.attr('transform','translate(' +self.w/2 +',' +self.h/2 +')rotate(90)')
-				/*.attr('transform',function(){
-					var x = self.w/2,
-						y = self.h/2;
-					return 'translate(' +x +',' +y +')';
-				})*/
 				;
+			hexG
+				.on('mouseout',function(d){
+					hexTTG.classed('hidden',true);
+				});
 			hexG.exit().remove();
-			hexes = hexG.selectAll('path.hex')
+			hexesG = hexG.selectAll('g.hexesG')
 				.data(function(d){ return d; });
-			hexes.enter().append('path')
-				.classed('hex',true);
-			hexes
-				.attr('d',hexbin.hexagon())
+			hexesG.enter().append('g')
+				.classed('hexesG',true);
+			hexesG
 				.attr('transform',function(d,i){
 					var x = d.pos.pixel.x ? d.pos.pixel.x : 0,
 						y = d.pos.pixel.y ? d.pos.pixel.y : 0;
 					return 'translate(' +x +',' +y +')';
-				})
+				});
+			hexesG.exit().remove();
+			hexes = hexesG.selectAll('path.hex')
+				.data(function(d){ return [d]; });
+			hexes.enter().append('path')
+				.classed('hex',true);
+			hexes
+				.attr('d',hexbin.hexagon())
 				.style('stroke',self.colors[self.mode])
 				.style('fill-opacity',function(d){
 					return d.rating/5;
 				});
+			hexes
+				.on('mousemove',function(d){
+					var x = d.pos.pixel.x ? d.pos.pixel.x : 0,
+						y = d.pos.pixel.y ? d.pos.pixel.y : 0;
+					var o = d.rating/5;
+					hexTTG
+						.classed('hidden',false)
+						.attr('transform',function(){
+							return 'translate(' +x +',' +y +')';
+						});
+					hexTT
+						.style('fill-opacity',o);
+				});
 			hexes.exit().remove();
+
+			//create tooltip group
+			hexTTG = hexG.selectAll('g.hexTTG')
+				.data([self.data]);
+			hexTTG.enter().append('g')
+				.classed('hexTTG',true);
+			hexTTG.exit().remove();
+			hexTTback = hexTTG.selectAll('path.hexTTback')
+				.data(function(d){ return [d]; });
+			hexTTback.enter().append('path')
+				.classed('hexTTback',true);
+			hexTTback
+				.attr('d',hexbinLG.hexagon())
+				.style('fill',self.colors[self.mode])
+				;
+			hexTTback.exit().remove();
+			hexTT = hexTTG.selectAll('path.hexTT')
+				.data(function(d){ return [d]; });
+			hexTT.enter().append('path')
+				.classed('hexTT',true);
+			hexTT
+				.attr('d',hexbinLG.hexagon())
+				.style('stroke',self.colors[self.mode]);
+			hexTT.exit().remove();
 		},
 		resize:function(){
 			//**TODO
